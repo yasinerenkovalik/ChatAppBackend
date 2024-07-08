b10da3974b337d2b893fe1d6f12f97873fa5b59c
Yasin Eren Kovalık <yasinerenkovalik@Yasins-MacBook-Air.local>
2024-07-08 17:37:35 +0300

    first commit

---
+++ ChatApp/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp;
+[Route("api/[controller]")]
+[ApiController]
+public class HomeController:Controller
+{
+    [HttpGet]
+    public string Get(){
+        return "hello";
+    }
+
+}
